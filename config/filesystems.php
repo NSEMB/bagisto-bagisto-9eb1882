feb3efd673685a266430b51bc0cd08f18f1c1ed0
NSEMB <mbiemepro@outlook.fr>
2024-05-25 01:23:17 +0200

    modifi env

--- config/filesystems.php
+++ config/filesystems.php
@@ -54,7 +54,7 @@ return [
         ],
 
         'public' => [
-            'driver'     => 'local',
+            'driver'     => 'ftp',
             'root'       => storage_path('app/public'),
             'url'        => env('APP_URL').'/storage',
             'visibility' => 'public',
